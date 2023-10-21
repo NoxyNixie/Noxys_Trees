e458da76597e330e59b1d61bd25d73a3629a5d72
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2023-10-22 01:08:52 +0200

    Option to control fire spread speed
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- data-updates.lua
+++ data-updates.lua
@@ -2,6 +2,13 @@ if settings.startup["Noxys_Trees-tree_dying_factor"].value > 0.00001 then
 	data.raw.fire["fire-flame-on-tree"].tree_dying_factor = settings.startup["Noxys_Trees-tree_dying_factor"].value
 end
 
+if settings.startup["Noxys_Trees-tree_fire_spread_speed_factor"].value ~= 1.0 then
+	local val = settings.startup["Noxys_Trees-tree_fire_spread_speed_factor"].value
+	local f = data.raw.fire["fire-flame-on-tree"]
+	f.spread_delay = f.spread_delay / val
+	f.spread_delay_deviation = f.spread_delay_deviation / val
+end
+
 asd = dsa
  
 local mul = settings.startup["Noxys_Trees-emission_multiplier"].value
